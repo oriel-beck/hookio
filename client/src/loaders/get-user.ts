38287a21966ee75784686c2b93ba93744df3d13b
oriel-beck <orielbeck@gmail.com>
2024-01-31 13:26:19 +0200

    make get-user load readable

--- client/src/loaders/get-user.ts
+++ client/src/loaders/get-user.ts
@@ -4,7 +4,33 @@ export default async function getUser({ request }: { request: Request }) {
     const url = new URL(request.url);
     const code = url.searchParams.get("code");
     if (code) {
-        return defer({ user: fetch(`/api/users/authenticate/${code}`, { method: "POST" }).catch(() => fetch("/api/users/current").catch(() => null)).then((r) => r?.json()) })
+        return defer({ user: authenticateAndFallback(code) })
     }
-    return defer({ user: fetch("/api/users/current").catch(() => null).then((r => r?.json())) });
+    return defer({ user: authenticateAndFallback(code) });
+}
+
+function authenticate(code: string) {
+    return fetch(`/api/users/authenticate/${code}`, { method: "POST" })
+}
+
+function getCurrentUser() {
+    return fetch("/api/users/current");
+}
+
+function authenticateAndFallback(code: string | null) {
+    // if there is a code, try to authenticate, if not, get the current user
+    return code ? authenticate(code)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            // if auth returned false, try to get the currently authenticated user anyways
+            return json ? json : getCurrentUser()
+                .then((response) => {
+                    return response.json()
+                })
+                .catch(() => null)
+        }) : getCurrentUser()
+            .then((r) => r.json())
+            .catch(() => null);
 }
