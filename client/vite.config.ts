8d58f7f17cffd46df318b4dbce243eb436d7a4b8
oriel-beck <orielbeck@gmail.com>
2024-03-31 13:39:38 +0300

    add polyfills for the libs sanitize html uses

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), 
+      nodePolyfills({
+        include: ['path', 'url', 'fs'],
+        protocolImports: true,
+      }),
+    ],
     server: {
       proxy: {
         '/api': {
