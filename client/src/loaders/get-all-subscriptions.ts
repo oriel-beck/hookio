53f327c6458648c2edc0e1737d058a90f9396acb
oriel-beck <orielbeck@gmail.com>
2024-03-31 21:51:11 +0300

    add redirects in case something invalid was provided to the dynamic routes

--- client/src/loaders/get-all-subscriptions.ts
+++ client/src/loaders/get-all-subscriptions.ts
@@ -1,7 +1,8 @@
-import { defer } from "react-router-dom";
+import { defer, redirect } from "react-router-dom";
 
-export default function getAllSubscriptions({ params }: { params: Record<string, unknown> }) {
+export default function getAllSubscriptions({ params }: { params: Record<string, string | undefined> }) {
     const { serverId, provider } = params;
+    if (!['youtube', 'twitch'].includes(provider!)) return redirect("/");
     return defer({ subscriptions: fetch(`/api/subscriptions/${serverId}/${Provider[provider as keyof typeof Provider]}`).then((r) => r.json()).catch(() => null) })
 }
 
--- client/src/loaders/get-subscription.ts
+++ client/src/loaders/get-subscription.ts
@@ -1,5 +1,5 @@
-import { defer } from "react-router-dom";
+import { defer, redirect } from "react-router-dom";
 
 export default async function getSubscription({ params }: { params: Record<string, unknown> }) {
-    return defer({ subscription: fetch(`/api/subscriptions/${params.subscriptionId}`).then((r) => r.json()).catch(() => null) })
+    return defer({ subscription: fetch(`/api/subscriptions/${params.subscriptionId}`).then((r) => r.json()).catch(() => redirect("/")) })
 }
