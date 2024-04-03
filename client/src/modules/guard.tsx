fc87f2a11eaf68566b0b388ee6d3811ece9e3a56
oriel-beck <orielbeck@gmail.com>
2024-04-03 21:49:31 +0300

    redirect to /servers when trying to access guilds with no access

--- client/src/modules/guard.tsx
+++ client/src/modules/guard.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import type { User } from "../types/types";
 
 export default function LoginGuard({ children }: { children: JSX.Element }) {
     const user = useOutletContext() as User;
     const navigate = useNavigate();
+    const params = useParams();
     useEffect(() => {
         if (!user) return navigate("/", { replace: true });
+        if (params.serverId && user.guilds.findIndex((g) => g.id === params.serverId) === -1) return navigate("/servers", { replace: true })
     })
     return children;
 }
