01cf1ca58d14f29eeffd4cac4a0a6c460c581698
oriel-beck <orielbeck@gmail.com>
2024-11-12 17:11:02 +0200

    add OAuth2 configuration

---
+++ Hookio.Shared/Configuration/OAuth2.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Hookio.Shared.Configuration
+{
+    public class OAuth2
+    {
+        public required string ClientId { get; set; }
+        public required string ClientSecret { get; set; }
+        public required string RedirectURI { get; set; }
+        public required string Scopes { get; set; }
+    }
+}
--- Hookio.WebApi/Program.cs
+++ Hookio.WebApi/Program.cs
@@ -1,6 +1,8 @@
+using Discord.Rest;
 using Hookio.Data;
 using Hookio.DataManagers;
 using Hookio.DataManagers.Interfaces;
+using Hookio.Shared.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddPooledDbContextFactory<HookioContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("HookioContext")));
 builder.Services.AddSingleton<ISubscriptionManager, SubscriptionManager>();
+builder.Services.AddSingleton<DiscordRestClient>();
+
+builder.Services.Configure<OAuth2>(builder.Configuration.GetSection(nameof(OAuth2)));
 
 var app = builder.Build();
 
