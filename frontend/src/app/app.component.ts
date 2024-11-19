aadcb079ae5a09cee6c3d657e0807caf5141e400
oriel-beck <orielbeck@gmail.com>
2024-11-19 23:59:52 +0200

    add navigation loader

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,45 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent, RouterOutlet } from '@angular/router';
 import { LayoutComponent } from "./components/layout/layout.component";
 import { UserService } from './services/user/user.service';
+import { LoadingOverlayComponent } from "./components/loading-overlay/loading-overlay.component";
 
 @Component({
   selector: 'hookio-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, LoadingOverlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   readonly userService = inject(UserService);
+  private readonly router = inject(Router)
+
+  navigating = signal(false);
+
+  constructor() {
+    this.router.events.subscribe({
+      next: (ev) => {
+        console.log(ev)
+        if (ev instanceof NavigationStart) {
+          this.navigating.set(true)
+        }
+        if (ev instanceof NavigationEnd) {
+          this.navigating.set(false);
+        }
+
+        // Set loading state to false in both of the below events to hide the spinner in case a request fails
+        if (ev instanceof NavigationCancel) {
+          this.navigating.set(false);
+        }
+        if (ev instanceof NavigationError) {
+          this.navigating.set(false);
+        }
+      }
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
 }
---
+++ frontend/src/app/components/loading-overlay/loading-overlay.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
+@Component({
+  selector: 'hookio-loading-overlay',
+  standalone: true,
+  imports: [
+    ProgressSpinnerModule
+  ],
+  templateUrl: './loading-overlay.component.html',
+  styleUrl: './loading-overlay.component.scss',
+  host: {
+    'aria-hidden': 'true'
+  }
+})
+export class LoadingOverlayComponent {
+
+}
