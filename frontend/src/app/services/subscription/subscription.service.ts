c03d1fac9537c3864878a4335c774e751b7e879e
oriel-beck <orielbeck@gmail.com>
2024-12-07 20:38:58 +0200

    fix patching interface

--- frontend/src/app/services/subscription/subscription.service.ts
+++ frontend/src/app/services/subscription/subscription.service.ts
@@ -1,8 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpService } from '../http/http.service';
-import { subscription, Subscription } from '../../schemas/subscription.schema';
+import { subscription, type Subscription } from '../../schemas/subscription.schema';
 import { map, Observable } from 'rxjs';
-import { User } from '../../schemas/user.schema';
+import type { User } from '../../schemas/user.schema';
+import type { SubscriptionPatch } from '../../types/subscription-patch';
 
 // validate that real subscriptions are returned and parse their data
 const validateSubscription = (sub: Subscription) => subscription.parse(sub);
@@ -27,7 +28,7 @@ export class SubscriptionService {
     return this.httpService.post<Subscription>(`/api/subscriptions/${guildId}`, data).pipe(map(validateSubscription));
   }
 
-  public patchSubscription(guildId: GuildId, subscriptionId: number, data: Partial<Subscription>) {
+  public patchSubscription(guildId: GuildId, subscriptionId: number, data: SubscriptionPatch) {
     return this.httpService.patch<Subscription>(`/api/subscriptions/${guildId}/${subscriptionId}`, data).pipe(map(validateSubscription));
   }
 
